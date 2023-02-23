f94851c0e63b2daa8bb05b5db783c1833f4dc692
William Varela <wiligramasdev@gmail.com>
2023-02-23 23:41:34 +0100

    fase 1

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    wiliCard: [
+      "#1B1F79",
+      "#1B1F79",
+      "#292460",
+      "#513A3A",
+      "#56749F",
+      "#E10C0C",
+      "#E8E8E8",
+      "#FFFFFF",
+      "#FFFFFF",
+    ],
+    default: ["black", "gray"],
+  }
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+globalThis.setCardType = setCardType
